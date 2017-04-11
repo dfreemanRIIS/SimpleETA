8d5115a710f30c66179c8d8a9b0fa16aefab9a03
Joseph Herkness <jpherkness@gmail.com>
2017-04-11 10:36:42 -0400

    Step 1: Add bus table view

--- SimpleETA/AppDelegate.swift
+++ SimpleETA/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let viewController = ViewController()
+        let viewController = BusTableViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
---
+++ SimpleETA/BusTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  SimpleETA
+//
+//  Created by Joseph Herkness on 4/11/17.
+//  Copyright © 2017 Joseph Herkness. All rights reserved.
+//
+
+import UIKit
+
+class BusTableViewController: UITableViewController {
+    
+    let busses = ["Smart", "DDot", "Reflex"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Comapny"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "busCell")
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return busses.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "busCell", for: indexPath)
+        cell.textLabel?.text = busses[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 94
+    }
+    
+}
